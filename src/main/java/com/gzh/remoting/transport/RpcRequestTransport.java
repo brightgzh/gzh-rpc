1070fb7e264b9b29b06d5a805f727ab600025d97
brightgzh <136730498+brightgzh@users.noreply.github.com>
2025-02-24 20:44:03 +0800

    Delete src/main directory

--- src/main/java/com/gzh/remoting/transport/RpcRequestTransport.java
+++
@@ -1,9 +0,0 @@
-package com.gzh.remoting.transport;
-
-import com.gzh.extension.SPI;
-import com.gzh.remoting.dto.RpcRequest;
-
-@SPI
-public interface RpcRequestTransport {
-    Object sendRpsRequest(RpcRequest rpcRequest);
-}
